dba08a52f7547239d1d9239c02b846ab68ba216e
Aleqxan <inno.alex@ymail.com>
2023-03-24 22:21:22 +0100

    worked on some controller function

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,12 +1,30 @@
 package controller
 
 import (
-
+	"context"
+	"golang-resturant-management/database"
+	"log"
+	"net/http"
+	"time"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc{
 	return func(c *gin.Context){
-
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while listing the menu item"})
+		}
+		var allMenus []bson.Menuif err = result.All(ctx, &allMenus); err!=nil{
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
 	}
 }
 
